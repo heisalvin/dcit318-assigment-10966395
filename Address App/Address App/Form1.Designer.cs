7cf6fd5ef72cf87be34bece78be1f38fe2687999
Alvin Bernasko <102929718+heisalvin@users.noreply.github.com>
2024-07-09 13:20:57 +0000

    Address App

---
+++ Address App/Address App/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Address_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.saveTextBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(62, 82);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(309, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(62, 141);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(309, 20);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(62, 187);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(309, 20);
+            this.phoneTextBox.TabIndex = 2;
+            // 
+            // saveTextBox
+            // 
+            this.saveTextBox.Location = new System.Drawing.Point(295, 230);
+            this.saveTextBox.Name = "saveTextBox";
+            this.saveTextBox.Size = new System.Drawing.Size(75, 23);
+            this.saveTextBox.TabIndex = 3;
+            this.saveTextBox.Text = "Save";
+            this.saveTextBox.UseVisualStyleBackColor = true;
+            this.saveTextBox.Click += new System.EventHandler(this.saveTextBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveTextBox);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Button saveTextBox;
+    }
+}
+
---
+++ Address App/Address App/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Address_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveTextBox_Click(object sender, EventArgs e)
+        {
+            String Name = nameTextBox.Text;
+            String Email = emailTextBox.Text;
+            String PhoneNumber = phoneTextBox.Text;
+
+
+            MessageBox.Show($"Name: {Name} \nEmail: {Email} \nPhone Number: {PhoneNumber}", "Saved Info");
+        }
+    }
+}
